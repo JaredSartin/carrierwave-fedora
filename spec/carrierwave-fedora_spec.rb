114a8127cc95d939a9b4dea4fc2abf59a740fff3
Jared Sartin <jaredsartin@gmail.com>
2011-10-13 16:56:01 -0400

    fixing gem to be valid

--- spec/carrierwave-fedora_spec.rb
+++ spec/carrierwave-fedora_spec.rb
@@ -5,3 +5,18 @@ require 'spec_helper'
 class FedoraSpecUploader < CarrierWave::Uploader::Base
   storage :fedora
 end
+
+describe CarrierWave::Storage::Fedora do
+  
+  describe '#store!' do
+    before do
+      #Create connection/object
+    end
+
+    it 'should have a Rubdora connection'
+
+    it 'should upload the file to the object datastream'
+
+  end
+
+end
