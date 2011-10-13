d152216122dc2a0c38fa8f8a8049d9e183ed9b16
Jared Sartin <jaredsartin@gmail.com>
2011-10-13 15:45:02 -0400

    birth

---
+++ lib/carrierwave-fedora.rb
@@ -0,0 +1,7 @@
+require "carrierwave-fedora/version"
+
+module Carrierwave
+  module Fedora
+    # Your code goes here...
+  end
+end
---
+++ lib/carrierwave-fedora/version.rb
@@ -0,0 +1,5 @@
+module Carrierwave
+  module Fedora
+    VERSION = "0.0.1"
+  end
+end
